031d0e176cce739dc51bcbfe8465cb12a9031d30
Justin Van Bibber <1976577+jvb93@users.noreply.github.com>
2020-06-01 09:05:37 -0700

    firebase function auth is go

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const axios = require("axios");
+const qs = require("qs");
 
 const cors = require("cors")({
   origin: true
@@ -8,32 +9,38 @@ const cors = require("cors")({
 exports.authenticate = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
     let data = request.body;
+
     let authRequest = {
       grant_type: "authorization_code",
       code: data.code,
       redirect_uri: data.redirect_uri
     };
 
+    let encoded = qs.stringify(authRequest);
     let concatTokens = `${functions.config().spotify.clientid}:${
       functions.config().spotify.clientsecret
     }`;
 
     let spotifyHeaders = {
       authorization: `Basic ${Buffer.from(concatTokens).toString("base64")}`,
-      "content-type": `application-json`
+      "content-type": `application/x-www-form-urlencoded`
     };
 
-    axios
-      .post("https://accounts.spotify.com/api/token", authRequest, {
-        headers: spotifyHeaders
-      })
-      .then(resp => {
-        response.status(200).send(resp.data);
-        return 0;
-      })
-      .catch(err => {
-        console.log(err);
-        return 1;
-      });
+    return new Promise((resolve, reject) => {
+      axios
+        .post("https://accounts.spotify.com/api/token", encoded, {
+          headers: spotifyHeaders
+        })
+        .then(res => {
+          response.status(200).send(res.data);
+          resolve();
+          return 0;
+        })
+        .catch(e => {
+          console.error(e);
+          response.status(500).send({});
+          reject(e);
+        });
+    });
   });
 });
