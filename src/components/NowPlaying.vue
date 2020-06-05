368b6005e00d7ff592db1e9adaefe9ee8a286d6c
Justin Van Bibber <1976577+jvb93@users.noreply.github.com>
2020-06-05 08:28:17 -0700

    added 1 hour session timeout

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -1,137 +1,151 @@
 <template>
-  <q-page v-if="accessToken">
-    <h4
-      class="text-center q-mb-none font-anton text-uppercase text-italic"
-      v-if="currentTrack"
-    >
-      Now Playing
-    </h4>
-    <template v-if="currentTrack && currentTrackFeatures">
-      <div class="row">
-        <div class="col">
-          <q-card flat bordered class="q-ma-lg">
-            <q-card-section>
-              <div class="row">
-                <div class="col-xs-12 col-md-4 text-center q-pa-md">
-                  <div class="row">
-                    <div class="col">
-                      <track-info :track="currentTrack.item" />
+  <q-page>
+    <div v-if="!sessionExpired">
+      <h4
+        class="text-center q-mb-none font-anton text-uppercase text-italic"
+        v-if="currentTrack"
+      >
+        Now Playing
+      </h4>
+      <template v-if="currentTrack && currentTrackFeatures">
+        <div class="row">
+          <div class="col">
+            <q-card flat bordered class="q-ma-lg">
+              <q-card-section>
+                <div class="row">
+                  <div class="col-xs-12 col-md-4 text-center q-pa-md">
+                    <div class="row">
+                      <div class="col">
+                        <track-info :track="currentTrack.item" />
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                        <track-control
+                          :track-id="currentTrack.item.id"
+                          :is-playing="currentTrack.is_playing"
+                          @refresh="getCurrentTrack"
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col">
-                      <track-control
-                        :track-id="currentTrack.item.id"
-                        :is-playing="currentTrack.is_playing"
-                        @refresh="getCurrentTrack"
-                      />
-                    </div>
+                  <div class="col-xs-12 col-md-8 q-pa-md">
+                    <track-list
+                      :tracks="currentAlbumTracks"
+                      :now-playing-id="currentTrack.item.id"
+                      :title="currentTrack.item.album.name"
+                    />
                   </div>
                 </div>
-                <div class="col-xs-12 col-md-8 q-pa-md">
-                  <track-list
-                    :tracks="currentAlbumTracks"
-                    :now-playing-id="currentTrack.item.id"
-                    :title="currentTrack.item.album.name"
-                  />
-                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <div class="row">
+              <div class="col">
+                <q-card flat bordered class="q-ma-lg">
+                  <q-card-section>
+                    <artist-info
+                      :id="currentTrack.item.artists[0].id"
+                      :now-playing-id="currentTrack.item.id"
+                    />
+                  </q-card-section>
+                </q-card>
               </div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
-          <div class="row">
-            <div class="col">
-              <q-card flat bordered class="q-ma-lg">
-                <q-card-section>
-                  <artist-info
-                    :id="currentTrack.item.artists[0].id"
-                    :now-playing-id="currentTrack.item.id"
-                  />
-                </q-card-section>
-              </q-card>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="row" v-if="genius.song">
+              <div class="col">
+                <q-card flat bordered class="q-ma-lg">
+                  <q-card-section
+                    v-if="genius.song.description.html != '<p>?</p>'"
+                  >
+                    <div
+                      class="text-h5 font-anton text-uppercase text-italic q-mb-md"
+                    >
+                      Track Description
+                    </div>
+                    <div
+                      class="flat-links img-small"
+                      style="overflow-x: auto;"
+                      v-html="genius.song.description.html"
+                    ></div>
+                  </q-card-section>
+                  <q-card-section>
+                    <div
+                      class="text-h5 font-anton text-uppercase text-italic"
+                      v-if="
+                        genius.song.release_date_for_display ||
+                          genius.song.recording_location
+                      "
+                    >
+                      Facts
+                    </div>
+                    <ul>
+                      <li v-if="genius.song.release_date_for_display">
+                        Release Date: {{ genius.song.release_date_for_display }}
+                      </li>
+                      <li v-if="genius.song.recording_location">
+                        Recording Location: {{ genius.song.recording_location }}
+                      </li>
+                    </ul>
+                  </q-card-section>
+                  <q-separator />
+                  <q-card-actions v-if="genius.song.url">
+                    <q-btn
+                      flat
+                      color="primary"
+                      type="a"
+                      :href="genius.song.url"
+                      target="_blank"
+                    >
+                      Read More on Genius.com
+                    </q-btn>
+                  </q-card-actions>
+                </q-card>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <q-card flat bordered class="q-ma-lg">
+                  <q-card-section>
+                    <div class="text-h5 font-anton text-uppercase text-italic">
+                      Track Analysis
+                    </div>
+                    <track-analysis :features="currentTrackFeatures" />
+                  </q-card-section>
+                </q-card>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-sm-12 col-md-6">
-          <div class="row" v-if="genius.song">
-            <div class="col">
-              <q-card flat bordered class="q-ma-lg">
-                <q-card-section
-                  v-if="genius.song.description.html != '<p>?</p>'"
-                >
-                  <div
-                    class="text-h5 font-anton text-uppercase text-italic q-mb-md"
-                  >
-                    Track Description
-                  </div>
-                  <div
-                    class="flat-links img-small"
-                    style="overflow-x: auto;"
-                    v-html="genius.song.description.html"
-                  ></div>
-                </q-card-section>
-                <q-card-section>
-                  <div
-                    class="text-h5 font-anton text-uppercase text-italic"
-                    v-if="
-                      genius.song.release_date_for_display ||
-                        genius.song.recording_location
-                    "
-                  >
-                    Facts
-                  </div>
-                  <ul>
-                    <li v-if="genius.song.release_date_for_display">
-                      Release Date: {{ genius.song.release_date_for_display }}
-                    </li>
-                    <li v-if="genius.song.recording_location">
-                      Recording Location: {{ genius.song.recording_location }}
-                    </li>
-                  </ul>
-                </q-card-section>
-                <q-separator />
-                <q-card-actions v-if="genius.song.url">
-                  <q-btn
-                    flat
-                    color="primary"
-                    type="a"
-                    :href="genius.song.url"
-                    target="_blank"
-                  >
-                    Read More on Genius.com
-                  </q-btn>
-                </q-card-actions>
-              </q-card>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <q-card flat bordered class="q-ma-lg">
-                <q-card-section>
-                  <div class="text-h5 font-anton text-uppercase text-italic">
-                    Track Analysis
-                  </div>
-                  <track-analysis :features="currentTrackFeatures" />
-                </q-card-section>
-              </q-card>
-            </div>
-          </div>
-        </div>
-      </div>
-    </template>
+      </template>
 
-    <div class="row justify-center window-height items-center" v-else>
-      <div class="text-center">
-        <q-spinner-audio color="primary" size="3em" />
-        <div class="text-h5 font-anton text-uppercase text-italic">
-          Looking for Audio...
+      <div class="row justify-center window-height items-center" v-else>
+        <div class="text-center">
+          <q-spinner-audio color="primary" size="3em" />
+          <div class="text-h5 font-anton text-uppercase text-italic">
+            Looking for Audio...
+          </div>
+          <div class="text-subtitle1">
+            Please note only music is supported at this time
+          </div>
         </div>
-        <div class="text-subtitle1">
-          Please note only music is supported at this time
-        </div>
+      </div>
+    </div>
+    <div v-else class="row items-center fullscreen">
+      <div class="col text-center">
+        <q-btn
+          @click="reload()"
+          align="between"
+          class="btn-fixed-width"
+          color="primary"
+          label="Session Expired, click to refresh"
+          icon="refresh"
+        />
       </div>
     </div>
   </q-page>
@@ -156,13 +170,14 @@ export default {
   },
   data() {
     return {
+      poller: null,
+      sessionExpired: false,
       accessToken: null,
       currentTrack: null,
       currentTrackFeatures: null,
       currentAlbumTracks: [],
       currentArtist: null,
       currentArtistPopularTracks: [],
-      poller: null,
       genius: {
         searchResults: [],
         song: null,
@@ -265,6 +280,9 @@ export default {
     },
     sanitizeString(inputString) {
       return inputString.toLowerCase().replace(/[\u200B-\u200D\uFEFF]/g, "");
+    },
+    reload() {
+      location.reload();
     }
   },
   created() {
@@ -275,10 +293,17 @@ export default {
       this.getCurrentTrack();
     });
 
+    let pollExpiration = new Date();
+    pollExpiration.setHours(pollExpiration.getHours() + 1);
+
     this.poller = setInterval(() => {
       GetTokenInfo().then(token => {
         this.accessToken = token.accessToken;
         this.getCurrentTrack();
+        if (new Date() > pollExpiration) {
+          clearInterval(this.poller);
+          this.sessionExpired = true;
+        }
       });
     }, 5000);
   }
